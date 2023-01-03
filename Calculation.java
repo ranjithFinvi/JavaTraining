f380bc6b2f3f372472af1b1497161cd885542a6d
Shreenaath@Ashwin <55404348+Shreenaath-15@users.noreply.github.com>
2023-01-03 15:44:31 +0530

    code updated

--- Calculation.java
+++ Calculation.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class Calculation {
     public static void main (String args[]){
+        int intput1, input2,result;
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter two Values:");
         int a = scan.nextInt();
         int b = scan.nextInt();
         float c = a - b;
-        System.out.println(c);
+            intput1=20;
+            input2=30;
+            result=intput1*input2;
+            System.out.println(c);
+            System.out.println(result);
+            
+        }
     }
-
-public class Calculation {
-
-	public static void main ( String args []){
-		System.out.println("multiplication of 4 * 5 = " + multiplication(4, 5, "*"))
-	}	
-	public int multiplication (int input1, int input2, String operand) {
-		return input1 * input2;
-	}
-}
